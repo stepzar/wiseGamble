101938ae0f07bed7b6e399fd02a3e39d984cf126
AurySepe <aureliosepe01@gmail.com>
2022-02-17 22:05:42 +0100

    fix classification

--- predictions/PredictionMakerSingleton.py
+++ predictions/PredictionMakerSingleton.py
@@ -9,8 +9,8 @@ encoder = preprocessing.LabelEncoder()
 encoder.classes_ = np.load('predictions/fileimport/encoder.npy')
 
 
-predictorYellowCard = Predictor("YellowCards", classificatore=load('/fileimport/yellowcardOvUn55.joblib'), scaler= load('predictions/fileimport/scaler_yellowcardsOvUn55.joblib'), encoder=encoder ,mappingOfResults={0: "Under",1:"Over"}, columns=["timestamp", "country", "all_ball_possession_away", "all_shots_on_target_home","all_shots_off_target_home","all_corner_kicks_home","all_corner_kicks_away", "all_goalkeeper_saves_away", "people_vote_x", "people_vote_2"])
-predictorCorner = Predictor("Corner", classificatore=load('/fileimport/cornerovun75.joblib'), scaler=load('predictions/fileimport/scaler_cornerovun75.joblib'), encoder=encoder, mappingOfResults={0: "Under", 1: "Over"}, columns=[])
+predictorYellowCard = Predictor("YellowCards", classificatore=load('predictions/fileimport/yellowcardsOvUn55.joblib'), scaler= load('predictions/fileimport/scaler_yellowcardsOvUn55.joblib'), encoder=encoder ,mappingOfResults={0: "Under 5.5",1:"Over 5.5"}, columns=["tournament_name", "country", "referee", "homeTeam_id", "awayTeam_id", "all_shots_on_target_home", "all_fouls_home", "all_fouls_away", "all_yellow_cards_home", "all_yellow_cards_away"])
+predictorCorner = Predictor("Corner", classificatore=load('predictions/fileimport/cornerovun75.joblib'), scaler=load('predictions/fileimport/scaler_cornerovun75.joblib'), encoder=encoder, mappingOfResults={0: "Under 7.5", 1: "Over 7.5"}, columns=["timestamp", "country", "all_ball_possession_away", "all_shots_on_target_home","all_shots_off_target_home","all_corner_kicks_home","all_corner_kicks_away", "all_goalkeeper_saves_away", "people_vote_x", "people_vote_2"])
 
 predictionMaker.addPredictor(predictorYellowCard)
 predictionMaker.addPredictor(predictorCorner)
--- predictions/Predictor.py
+++ predictions/Predictor.py
@@ -15,25 +15,42 @@ class Predictor:
         self.columns = columns
 
     def predictPartita(self, partita : Partita):
-        print("partita df " , partita.df)
-        self.encoding(partita.df)
-        print("partita df dopo encoding ", partita.df)
-        df = self.scaling(partita.df)
-        print("partita df dopo scaling ", df)
+        df = self.createDataframe(partita)
+        self.encoding(df)
+        df = self.scaling(df)
+        df = df[self.columns]
         prediction = self.classificatore.predict(df)
         prediction = prediction[0]
         return (self.outputName, self.mappingOfResults[prediction])
 
     def encoding(self, df):
-        df["tournament_name"] = self.encoder.fit_transform(df.tournament_name)
-        df["country"] = self.encoder.fit_transform(df.country)
-        df["city"] = self.encoder.fit_transform(df.city)
-        df["stadium"] = self.encoder.fit_transform(df.stadium)
-        df["referee"] = self.encoder.fit_transform(df.referee)
-        df["home_formation"] = self.encoder.fit_transform(df.home_formation)
-        df["away_formation"] = self.encoder.fit_transform(df.away_formation)
+        if("tournament_name" in df.columns):
+            df["tournament_name"] = self.encoder.fit_transform(df.tournament_name)
+        if ("country" in df.columns):
+            df["country"] = self.encoder.fit_transform(df.country)
+        if ("referee" in df.columns):
+            df["referee"] = self.encoder.fit_transform(df.referee)
 
     def scaling(self, df):
         df_scaling = self.scaler.transform(df)
         df_scaling = DataFrame(df_scaling, columns=df.columns)
         return df_scaling
+
+    def createDataframe(self, partita):
+        cols = ['timestamp', 'tournament_name', 'country', 'round', 'city', 'stadium',
+       'referee', 'homeTeam_id', 'awayTeam_id', 'all_ball_possession_home',
+       'all_ball_possession_away', 'all_shots_on_target_home',
+       'all_shots_on_target_away', 'all_shots_off_target_home',
+       'all_shots_off_target_away', 'all_corner_kicks_home',
+       'all_corner_kicks_away', 'all_fouls_home', 'all_fouls_away',
+       'all_yellow_cards_home', 'all_yellow_cards_away',
+       'all_goalkeeper_saves_home', 'all_goalkeeper_saves_away',
+       'people_vote_1', 'people_vote_x', 'people_vote_2', 'id_manager_home',
+       'id_manager_away']
+        df = pandas.DataFrame({k : [0] for k in cols })
+        for col in self.columns:
+            df.loc[0,col] = partita.__dict__[col]
+        #df = pandas.DataFrame({k: [v] for k, v in partita.__dict__.items()})
+        #return df[self.columns]
+        return df
+
